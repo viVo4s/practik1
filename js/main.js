1c900679b36719b3f12df32cc4c36b21a7768a0c
viVo4s <mr.stas22800@gmail.com>
2024-01-16 17:33:16 +0700

    4 commit

--- js/main.js
+++ js/main.js
@@ -13,13 +13,27 @@ let app = new Vue({
       variants: [
         {
           variantId: 2234,
-          variantColor: 'green'    
+          variantColor: 'green',
+          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
         },
         {
           variantId: 2235,
-          variantColor: 'blue'
+          variantColor: 'blue',
+          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
         }
       ],
-      sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+      sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+      cart: 0
+    },
+    methods: {
+      addToCart() {
+        this.cart += 1
+      },
+      updateProduct(variantImage) {
+        this.image = variantImage
+      },
+      removeFromCart() {
+        this.cart -= 1
+      }
     }
   });

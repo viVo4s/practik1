254fb4d4924d2d343149bd391f8786f167389fd6
viVo4s <mr.stas22800@gmail.com>
2024-01-16 17:01:53 +0700

    2 commit

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let app = new Vue({
       product: 'Socks',
       description: 'A pair of warm fuzzy socks',
       image: "./assets/vmSocks-green-onWhite.jpg",
+      link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
       altText: "A pair of socks",
       inStock: true,
       inventory: 100

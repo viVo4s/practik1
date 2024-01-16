5ef61fec7e022a5d88d8ba871b97b107b7e6d1dd
viVo4s <mr.stas22800@gmail.com>
2024-01-16 16:58:27 +0700

    commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks",
-                description: 'A pair of warm fuzzy socks'
-
-        image: "./assets/vmSocks-green.jpg"
+      product: 'Socks',
+      description: 'A pair of warm fuzzy socks',
+      image: "./assets/vmSocks-green-onWhite.jpg",
+      altText: "A pair of socks",
+      inStock: true,
+      inventory: 100
     }
-})
+  });
